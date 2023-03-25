32ba0f3f3efd00642f779e5966b20b24fc979b89
KobzarAnatolii <TohaPirat@gmail.com>
2023-03-25 22:24:39 +0200

    done

--- src/components/TaskForm/TaskForm.js
+++ src/components/TaskForm/TaskForm.js
@@ -1,10 +1,18 @@
-import { Button } from "components/Button/Button";
-import css from "./TaskForm.module.css";
+import { Button } from 'components/Button/Button';
+import { useDispatch } from 'react-redux';
+import css from './TaskForm.module.css';
+import { addTask } from 'redux/actions';
 
 export const TaskForm = () => {
-  const handleSubmit = (event) => {
+  const dispatch = useDispatch();
+
+  const handleSubmit = event => {
     event.preventDefault();
     const form = event.target;
+    // Вызываем генератор экшена и передаем текст задачи для поля payload
+    // Отправляем результат - экшен создания задачи
+    dispatch(addTask(form.elements.text.value));
+
     form.reset();
   };
 
